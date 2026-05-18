d3de667e3d6c75292120a7ef34809757a5ff217a
khoa75 <nguyenhuuanhkhoa.dev@gmail.com>
2026-05-18 12:30:43 +0700

    Delete old backend

--- backend/src/main/java/com/nutrition/common/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.nutrition.common.exception;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
